6c38a012d8fcc3e93e556c392fb6503c63307b10
Esaias Cronelius <esaias@cronelius.se>
2023-05-19 20:40:07 +0200

    Start from the bottom

--- app/src/components/messages/MessageList.tsx
+++ app/src/components/messages/MessageList.tsx
@@ -1,12 +1,30 @@
-import { View } from 'react-native'
+import { useRef } from 'react'
+import { ScrollView, View } from 'react-native'
 import Message from './Message'
 
 const MessageList = () => {
+  const scrollViewRef = useRef<ScrollView>(null)
+
   return (
     <View className='flex-1 justify-end'>
-      <Message />
-      <Message />
-      <Message />
+      <ScrollView ref={scrollViewRef} onContentSizeChange={() => scrollViewRef.current?.scrollToEnd({ animated: false })}>
+        <Message />
+        <Message />
+        <Message />
+        <Message />
+        <Message />
+        <Message />
+        <Message />
+        <Message />
+        <Message />
+        <Message />
+        <Message />
+        <Message />
+        <Message />
+        <Message />
+        <Message />
+        <Message />
+      </ScrollView>
     </View>
   )
 }
