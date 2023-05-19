abc51759ee14abaa2e25936a59833b456e7b1815
Esaias Cronelius <esaias@cronelius.se>
2023-05-19 18:00:50 +0200

    Added CORS

--- backend/src/services/server.ts
+++ backend/src/services/server.ts
@@ -1,8 +1,10 @@
 import { createHTTPServer } from '@trpc/server/adapters/standalone'
+import cors from 'cors'
 import { appRouter } from '../routers/app.js'
 
 export const createServer = async () => {
   const server = createHTTPServer({
+    middleware: cors(),
     router: appRouter,
     createContext: () => {
       return {}
